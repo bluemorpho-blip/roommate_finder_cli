8286a110840c57110932b817c11a751ace17e4fd
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-10 03:00:50 +0000

    scraper built

--- Rakefile
+++ Rakefile
@@ -1,9 +1,18 @@
 require_relative './config/environment'
 
-def reload
+def reload!
   load.all './lib'
 end
 
 task :console do
-  pry.start
+  Pry.start
+end
+
+task :scrape_rooms do
+# instantiate a scraper and then have it find new rooms
+  nyc_scraper = RoomScraper.new('https://newyork.craigslist.org/search/roo')
+  # nyc_scraper.call
+  # chicago_scraper = RoomScraper.new('https://chicago.craigslist.org/search/roo')
+  # chicago_scraper.call
+  # after this method call I should be able to say Room.all and have rooms there.
 end
--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -1,3 +1,33 @@
 class RoomScraper
 
+  def initialize(index_url)
+    @index_url = index_url
+    @doc = Nokogiri::HTML(open(index_url))
+  end
+
+  def call
+    rows.each do |row_doc|
+      Room.create_from_hash(scrape_row(row_doc)) #=> Should put the room in my database.
+    end
+  end
+
+  private
+    def rows
+      @rows ||= @doc.search('div.content li.result-row')
+    end
+
+    def scrape_row(row)
+      # scrape an individual row
+      {
+        :date_created => row.search("time.result-date").text,
+        :title => row.search("a.hdrlnk").text,
+        :url => row.search("a.hdrlnk").attribute("href").text,
+        :price => row.search("span.result-meta span.result-price").text
+      }
+    end
+
+
+
+
+
 end
