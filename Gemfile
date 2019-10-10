0d48e69caf660f8d35d29126528bce47408ee08e
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-10 22:53:08 +0000

    active record added

--- Gemfile
+++ Gemfile
@@ -6,5 +6,5 @@ gem 'nokogiri'
 gem 'pry'
 gem 'require_all'
 gem 'sqlite3'
-
+gem 'activerecord', :require => 'active_record'
 # gem "rails"
--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,13 @@ Bundler.require
 
 require 'open-uri'
 
-DB = {
-  :connection => SQLite3::Database.new('db/rooms_development.sqlite')
-}
+# DB = {
+#   :connection => SQLite3::Database.new('db/rooms_development.sqlite')
+# }
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'db/rooms_development.sqlite'
+)
 
 require_all 'lib'
